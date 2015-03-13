291b30a7cd29690c292e8c5ec1657845ccd91a15
Will Keeling <will@zifferent.com>
2015-03-13 20:27:53 +0000

    adding distance and re-ordering stops on location update

--- app/shared/stopLocationService.js
+++ app/shared/stopLocationService.js
@@ -4,47 +4,37 @@ bustimes.service('StopLocationService', ['$q', 'DataService', StopLocationServic
 function StopLocationService($q, DataService) {
     'use strict';
     
-    var that = this;
+    var LOCATION_UPDATE = 'location_update',
+        that = this;
     
-    // this.getPosition = function() {
-        // var deferred = $q.defer();
-        // if (navigator.geolocation) {
-            // var options = {
-                // timeout : 5000,
-                // maximumAge : 0
-            // }; 
-        // } else {
-            // console.warn('Browser does not support geolocation');
-            // resolveManually(limit, excluding, deferred);
-        // }
-//         
-        // return deferred.promise;        
-    // }
-    
-    this.getStopsNearestMe = function(limit, excluding) {
-        var deferred = $q.defer();
-        
+    this.updateLocation = function() {
         if (navigator.geolocation) {
             var options = {
                 timeout : 5000,
                 maximumAge : 0
-            }; 
+            };         
             var watchId = navigator.geolocation.watchPosition(function(pos) {
                 navigator.geolocation.clearWatch(watchId);
-                getStopsNearest(pos, limit, excluding).then(function(stops) {
-                    deferred.resolve(stops);
-                });
+                
+                DataService.getStops().then(function(stops) {
+                    stops.forEach(function(stop) {
+                        var dist = getDistanceFromLatLonInKm(stop.latitude, stop.longitude, 
+                                                             pos.coords.latitude, pos.coords.longitude);
+                        stop.distance = dist;
+                    });
+                    stops.sort(function(stop1, stop2) {
+                        return stop1.distance - stop2.distance;
+                    });
+                });                
+                
             }, function(err) {
                 console.warn('Unable to get current position: ' + err.message);
-                resolveManually(limit, excluding, deferred);
             }, options);
         } else {
             console.warn('Browser does not support geolocation');
-            resolveManually(limit, excluding, deferred);
-        }
-        
-        return deferred.promise;
-    }; 
+        }            
+    };
+    
     
     // Manually set location, perhaps by bringing up a map
     // Publically exposed as used elsewhere
@@ -54,59 +44,23 @@ function StopLocationService($q, DataService) {
         return deferred.promise;
     };
     
-    function resolveManually(limit, excluding, deferred) {
-        that.setMyLocation().then(function(position) {
-            getStopsNearest(position, limit, excluding).then(function(stops) {
-                deferred.resolve(stops);
-            });
-        });
+    function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+        var R = 6371;
+        // Radius of the earth in km
+        var dLat = deg2rad(lat2 - lat1);
+        // deg2rad below
+        var dLon = deg2rad(lon2 - lon1);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        // Distance in km
+        return d;
     }
     
-    function getStopsNearest(pos, limit, excluding) {
-        var deferred = $q.deferred;
-        DataService.getStops().then(function(stops) {
-            var nearestStops = [];
-            // Do logic. ETADirective
-            deferred.resolve(nearestStops);
-        });
-        return deferred.promise;
+    function deg2rad(deg) {
+        return deg * (Math.PI / 180);
     }
+    
+    this.updateLocation();
 }
 
-function getNearestStop(func) {
-    if (navigator.geolocation) {
-        var watchId = navigator.geolocation.watchPosition(function(pos) {   
-            navigator.geolocation.clearWatch(watchId);
-            var lastDist = -1,
-                lastStop = null;
-            stops.forEach(function(stop) {
-                var dist = getDistanceFromLatLonInKm(stop.latitude, stop.longitude, pos.coords.latitude, pos.coords.longitude);
-                console.log(stop.name + ': ' + dist);
-                if (dist < lastDist || lastDist == -1) {
-                    lastDist = dist;
-                    lastStop = stop;
-                }
-            });
-            func(lastStop);
-        });
-    } else {
-        throw 'Requires geolocation support';
-    }
-}
-
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    var R = 6371;
-    // Radius of the earth in km
-    var dLat = deg2rad(lat2 - lat1);
-    // deg2rad below
-    var dLon = deg2rad(lon2 - lon1);
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    // Distance in km
-    return d;
-}
-
-function deg2rad(deg) {
-    return deg * (Math.PI / 180);
-}
