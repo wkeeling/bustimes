fd720584f86d65fe26d134cb566bb7a86aea7b7d
Will Keeling <will@zifferent.com>
2015-03-21 22:08:22 +0000

    Removed old file structure

--- static/app/app.module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var bustimes = angular.module('bustimes', ['ngAnimate', 'ui.bootstrap', 'ui.utils']);
-
-bustimes.run(['DataService', 'StopLocationService', function(DataService, StopLocationService) {
-    DataService.init();
-    StopLocationService.updateLocation();
-}]);
--- static/app/shared/utils.js
+++
@@ -1,9 +0,0 @@
-bustimes.directive('focusOn', function() {
-    'use strict';
-    return function(scope, elem, attr) {
-        scope.$on(attr.focusOn, function(e) {
-            elem[0].focus();
-        });
-    };
-}); 
-
